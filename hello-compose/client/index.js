b1920cb0f5af36d26fff73d4838f5482ed938571
Brian Burns <bburns.km@gmail.com>
2021-03-02 11:22:04 -0600

    wip

--- hello-compose/client/index.js
+++ hello-compose/client/index.js
@@ -4,13 +4,13 @@ const host = process.env.HOST || 'localhost'
 const port = Number(process.env.PORT || 7878)
 const config = { host, port }
 
-console.log(`Creating socket...`)
+console.log(`Creating client socket...`)
 const socket = new net.Socket()
-// const socket = net.createConnection(config)
+
 console.log(`Connecting to server`, config, `...`)
 socket.connect(config, () => {
   console.log(`Sending text...`)
-  socket.write('Hello')
+  socket.write('Hello, server')
   console.log(`Ending socket...`)
   socket.end()
 })
--- hello-compose/server/index.js
+++ hello-compose/server/index.js
@@ -5,39 +5,32 @@ const port = Number(process.env.PORT || 7878)
 const config = { host, port }
 
 console.log(`Creating TCP server`, config, '...')
-const server = net.createServer(socket => {
-  // socket.on('data', function (data) {
-  //   console.log(`Received data:`)
-  //   console.log(data.toString())
-  //   // socket.end()
-  // })
-  // console.log(`Someone connected...`)
-})
+const server = net.createServer()
 
 server.on('connection', function (socket) {
   console.log('A new connection has been established.')
 
-  // Now that a TCP connection has been established, the server can send data to
-  // the client by writing to its socket.
+  // send data to client on the socket
   socket.write('Hello, client.')
 
-  // The server can also receive data from the client by reading from its socket.
+  // receive data from client on the socket
   socket.on('data', function (chunk) {
     console.log(`Data received from client: ${chunk.toString()}`)
   })
 
   // When the client requests to end the TCP connection with the server, the server
   // ends the connection.
+  //. nowork?
   socket.on('end', function () {
     console.log('Closing connection with the client')
   })
 
-  // Don't forget to catch error, for your own sake.
+  // catch any errors
   socket.on('error', function (err) {
     console.log(`Error: ${err}`)
   })
 })
 
 server.listen(config, () => {
-  console.log(`Server listening on 7878`)
+  console.log(`Server listening on`, config)
 })
