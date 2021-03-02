a18f6d34ebc5d58b9e3ca6a0c2276ded19ef0f0b
Brian Burns <bburns.km@gmail.com>
2021-03-02 09:17:54 -0600

    wip

--- hello-compose/dest/index.js
+++ hello-compose/dest/index.js
@@ -1,47 +1,43 @@
 const net = require('net')
 
-// const host = process.env.HOST || 'localhost'
-// const port = Number(process.env.PORT || 7878)
-// const config = { host, port }
+const host = process.env.HOST || 'localhost'
+const port = Number(process.env.PORT || 7878)
+const config = { host, port }
 
-console.log(`Creating TCP server`)
-
+console.log(`Creating TCP server`, config, '...')
 const server = net.createServer(socket => {
-  socket.on('data', function (data) {
-    console.log(`Received data:`)
-    console.log(data.toString())
-  })
-
-  console.log(`Someone connected...`)
-  console.log(`Sending text...`)
-
-  socket.write(`Hello, world!\n`)
-
-  const buffer = Buffer.from('pokpok\n')
-  socket.write(buffer)
-
-  console.log(`Ending socket...`)
-  socket.end()
-
-  // net.connect(config, () => {
-  //   // If there is no error, the server has accepted the request and created a new
-  //   // socket dedicated to us.
-  //   console.log('TCP connection established with the server.')
-  //   // The client can now send data to the server by writing to its socket.
-  //   // client.write('Hello, server.');
+  // socket.on('data', function (data) {
+  //   console.log(`Received data:`)
+  //   console.log(data.toString())
+  //   // socket.end()
   // })
-
-  // console.log(`Sending SHDR to diode over TCP at`, config.diodeSender, `...`)
-  // const str = 'oijoij'
-  // socket.write(str)
-  // console.log(`Closing TCP...`)
+  // console.log(`Someone connected...`)
 })
 
-server.listen(7000, 'localhost')
-console.log(`Server listening on 7000`)
+server.on('connection', function (socket) {
+  console.log('A new connection has been established.')
 
-// // client
-// var s = new net.Socket()
-// s.connect(7000)
-// s.write('Hello')
-// s.end()
+  // Now that a TCP connection has been established, the server can send data to
+  // the client by writing to its socket.
+  socket.write('Hello, client.')
+
+  // The server can also receive data from the client by reading from its socket.
+  socket.on('data', function (chunk) {
+    console.log(`Data received from client: ${chunk.toString()}`)
+  })
+
+  // When the client requests to end the TCP connection with the server, the server
+  // ends the connection.
+  socket.on('end', function () {
+    console.log('Closing connection with the client')
+  })
+
+  // Don't forget to catch error, for your own sake.
+  socket.on('error', function (err) {
+    console.log(`Error: ${err}`)
+  })
+})
+
+server.listen(config, () => {
+  console.log(`Server listening on 7878`)
+})
